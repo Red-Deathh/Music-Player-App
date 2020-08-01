73109b7b56b7159cbde78fef605907e109d783c2
Red-Deathh <49135523+Red-Deathh@users.noreply.github.com>
2020-08-01 17:28:32 +0100

    Music Player App

--- app/src/main/java/com/derron/musicplayer/MainActivity.java
+++ app/src/main/java/com/derron/musicplayer/MainActivity.java
@@ -3,6 +3,7 @@ package com.derron.musicplayer;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -11,31 +12,40 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.viewpager);
+        PageAdapter adapter;
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new AllSongs(),null).commit();
+
+        adapter = new PageAdapter(getSupportFragmentManager(),bottomNavigationView.getMaxItemCount());
+        viewPager.setAdapter(adapter);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            Fragment fragment = null;
+            Fragment selectedfragment = null;
 
             switch (menuItem.getItemId()){
                 case R.id.songlist:
-                    fragment = new AllSongs();
+                    selectedfragment = new AllSongs();
+                    break;
                 case R.id.playlist:
-                    fragment = new Playlist();
+                    selectedfragment = new Playlist();
+                    break;
                 case R.id.album:
-                    fragment = new Album();
+                    selectedfragment = new Album();
                     break;
             }
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment,null).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, selectedfragment,null).commit();
             return true;
         }
     };
