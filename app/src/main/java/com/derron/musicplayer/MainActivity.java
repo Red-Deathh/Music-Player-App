4b57cc15216e493a475ee5c17f6eb21c6373e737
Chikuai <chikuai54@gmail.com>
2020-08-02 00:40:16 +0530

    Modify By Saurya

--- app/src/main/java/com/derron/musicplayer/MainActivity.java
+++ app/src/main/java/com/derron/musicplayer/MainActivity.java
@@ -1,9 +1,14 @@
 package com.derron.musicplayer;
 
+import android.animation.ObjectAnimator;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,8 +22,7 @@ import androidx.navigation.ui.NavigationUI;
 public class MainActivity extends AppCompatActivity {
 
 
-    Button pre, pp, next;
-    TextView songtxt;
+
 
 
     @Override
@@ -26,49 +30,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final MediaPlayer mpE = MediaPlayer.create(MainActivity.this, R.raw.test);
-
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_songs, R.id.navigation_playlist, R.id.navigation_album)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
-
-
-        pre = findViewById(R.id.btn_pre);
-        pp = findViewById(R.id.btn_pp);
-        next = findViewById(R.id.btn_next);
-        songtxt = findViewById(R.id.song_txt);
-
-        songtxt.setVisibility(View.INVISIBLE);
-
-
-        pp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (mpE.isPlaying()) {
-
-                    mpE.pause();
-                    pp.setBackgroundResource(R.drawable.ic_baseline_play_arrow_24);
-
-                } else {
-                    pp.setBackgroundResource(R.drawable.ic_baseline_pause_24);
-                    mpE.start();
-                    songtxt.setText("Kabhi Saam....");
-                    songtxt.setVisibility(View.VISIBLE);
-
-                }
-
-            }
-        });
-
 
     }
+
 
 }
